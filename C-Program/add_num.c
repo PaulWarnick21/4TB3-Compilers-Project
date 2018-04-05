1f930df219585bb284987cf75654542df08a8823
Randa <randamohsen@hotmail.ca>
2018-04-05 19:20:01 -0400

    taking out user input

--- C-Program/add_num.c
+++ C-Program/add_num.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-/* this program takes in 2 command line arguments
- * which must be integers and prints the sum of them.
+/* this program takes in 0 command line arguments which must be integers and prints the sum of them.
  */
 
 int main( int argc, char *argv[] )  {
-     int A = 0;
-     int B = 0;
-     sscanf(argv[1],"%d", &A);
-     sscanf(argv[2],"%d", &B);
-     printf("%d \n", A + B);
+     int A = 6;
+     int B = 8;
+     printf(" A + B is %d \n", A + B);
 }
