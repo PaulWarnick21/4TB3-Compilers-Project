4dac74d7e2029637188eed860fb970203e2bb723
Randa <randamohsen@hotmail.ca>
2018-04-05 22:01:58 -0400

    Simplified some more

--- C-Program/add_num.c
+++ C-Program/add_num.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/* this program takes in 0 command line arguments which must be integers and prints the sum of them.
- */
+/* this program prints the sum of 6 and 8 using integer variables. */
 
-int main( int argc, char *argv[] )  {
+int main ( int argc, char *argv[] )  {
      int A = 6;
      int B = 8;
-     printf(" A + B is %d \n", A + B);
+     printf( "A + B is %d \n", A + B);
 }
