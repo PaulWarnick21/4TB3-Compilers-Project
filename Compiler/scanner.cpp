767bcc31ba94d096550ad7f2ea873a1825db1a25
Randa <randamohsen@hotmail.ca>
2018-04-05 22:02:24 -0400

    print statements reflect how the scanner differentiates between types, names, and values

--- Compiler/scanner.cpp
+++ Compiler/scanner.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 #include "abstractSyntaxTree.h"
 
@@ -19,8 +20,13 @@ int numberOfChars;
 
 void getChar(std::ifstream& is);
 void getSymbol(std::ifstream& is);
-void getInclude(std::ifstream& is);
+// void getInclude(std::ifstream& is);
 void getUntilChar(std::ifstream& is, char i_ch);
+// int CompareChar(std::ifstream& is, char i_ch, char j_ch);
+
+string getDigitFromString(string input);
+
+bool inTheFunction = false;
 
 int  main(int argc, char *argv[]) {
   std::ifstream f(argv[1]);
@@ -34,7 +40,7 @@ int  main(int argc, char *argv[]) {
 void getChar(std::ifstream& is) {
   if (is.eof()) ch = '\0';
   else {
-    is >> ch;
+    is.get(ch);
     if (is.fail()) ch = '\0';
     // debugging purposes only
     // else std::cout << "Character read: " << ch << std::endl;
@@ -42,34 +48,119 @@ void getChar(std::ifstream& is) {
 }
 
 void getSymbol(std::ifstream& is) {
+
+  if (ch == '{') inTheFunction = true;
+
+  if (!inTheFunction){
+
+    getChar(is);
+    // break beacuse we are not in the function yet
+    getSymbol(is);
+  }
+
   switch (ch) {
-  case  0:
+
+    case  0:
     std::exit(EXIT_FAILURE);
 
-  case '#':
-		getInclude(is);
-    break;
-  case '<':
-    getUntilChar(is, '>');
-    std::cout << "File read: " << SolidString << std::endl;
-    // TODO: take variable SoldiString (holds the name of an include file)
+    /* the following is commented out because we are currently only concerned with keywords that appear inside functions, and "include" doesn't */
+
+    // case '#':
+    //   getChar(is);
+    // 	getUntilChar(is, ' ');
+    //   std::cout << "  KEYWORD:  " << SolidString << std::endl;
+    //   SolidString = "";
+    //   break;
+
+    /* anything incased in "<>" will be considered of file type and should be extracted as such*/
+
+    // case '<':
+    //   getChar(is);
+    //   getUntilChar(is, '>');
+    //   std::cout << "  FILE:     " << SolidString << std::endl;
+    //   // TODO: take variable SoldiString (holds the name of an include file)
+    //   SolidString = "";
+    //   break;
+
+    case 'c':
+    getUntilChar(is, ' ');
+    if (SolidString == "char") {
+      std::cout << "  TYPE:       " << SolidString << std::endl;
+      SolidString = "";
+      getChar(is);
+
+      getUntilChar(is, ' ');
+      // getChar(is);
+      std::cout << "  NAME:       " << SolidString << std::endl;
+
+    }
+
     SolidString = "";
     break;
-  case '/':
+
+    case 'p':
+
+    getUntilChar(is, '(');
+    if (SolidString.find("print") != std::string::npos) {
+
+      std::cout << "  FUNCTION:   " << SolidString << std::endl;
+      SolidString = "";
+      getChar(is);
+
+      getUntilChar(is, ')');
+      getChar(is);
+
+      std::cout << "  INPUTS:   " << SolidString << std::endl;
+      SolidString = "";
+      getChar(is);
+    }
+
+    SolidString = "";
+    break;
+
+    case 'i' :
+    getUntilChar(is, ' ');
+    if (SolidString == "int") {
+      std::cout << "  TYPE:       " << SolidString << std::endl;
+      SolidString = "";
+      getChar(is);
+
+      getUntilChar(is, ' ');
+      std::cout << "  NAME:       " << SolidString << std::endl;
+      SolidString = "";
+
+      getUntilChar(is, '=');
+      getChar(is);
+      SolidString = "";
+
+      getUntilChar(is, ' ');
+      getChar(is);
+      SolidString = "";
+
+      getUntilChar(is, ';');
+      std::cout << "  VALUE:      " << SolidString << std::endl;
+    }
+
+    SolidString = "";
+    break;
+
+    case '/':
     getChar(is);
     getUntilChar(is, '/');
-    std::cout << "Comment read: " << SolidString << std::endl;
-    // TODO: take variable SoldiString (holds the name of an include file)
+    std::cout << "  COMMENT:    " << SolidString << std::endl;
+    // SoldiString (holds the contents of the comment)
     SolidString = "";
     break;
-  case '"':
+
+    case '"':
     getChar(is);
     getUntilChar(is, '"');
-    std::cout << "String read: " << SolidString << std::endl;
-    // TODO: take variable SoldiString (holds the name of an include file)
+    std::cout << "  STRING:       " << SolidString << std::endl;
+    // SoldiString (holds the contents of the comment)
     SolidString = "";
     break;
-  default:
+
+    default:
     // randa += ch;
     // std::cout << "Word read: " << randa << std::endl;
     // std::cout << "Character read: " << ch << std::endl;
@@ -79,18 +170,17 @@ void getSymbol(std::ifstream& is) {
   getSymbol(is);
 }
 
-void getInclude(std::ifstream& is) {
-  for(int i = 0; i < 7; i++){
-		getChar(is);
-
-	}
-	// while(ch =){
-	//
-	// }
-}
+// void getInclude(std::ifstream& is) {
+//   for(int i = 0; i < 7; i++){
+// 		getChar(is);
+//
+// 	}
+// 	// while(ch =){
+// 	//
+// 	// }
+// }
 
 void getUntilChar(std::ifstream& is, char i_ch) {
-  getChar(is);
 
   for(int i = 0; i < 110; i++){
     if  (ch == i_ch) {
@@ -108,3 +198,24 @@ void getUntilChar(std::ifstream& is, char i_ch) {
   return;
 
 }
+
+/* The following is commented because we will not use it yet */
+// int CompareChar(std::ifstream& is, char i_ch, char j_ch) {
+//
+//   for(int i = 0; i < 110; i++){
+//     if  (ch == i_ch) {
+//       return 1;
+//     } if  (ch == j_ch) {
+//       return 2;
+//     } else {
+//       // std::cout << "Character read: " << ch << std::endl;
+//
+//       SolidString += ch;
+//       getChar(is);
+//     }
+//
+//   }
+//   // std::cout << "Word read: " << randa << std::endl;
+//
+//   return 0;
+// }
