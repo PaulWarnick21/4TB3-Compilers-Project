e9f250d0ccf2a87d7143086b2ba837d905c782fe
Navleen <navleen95@gmail.com>
2018-04-05 21:15:35 -0400

    Added some adding jasmin code

--- Compiler/abstractSyntaxTree.h
+++ Compiler/abstractSyntaxTree.h
@@ -2,21 +2,21 @@ typedef enum { Void, Int, Float, Double, String, Char, Bool } TYPE;
 typedef enum { Protected, Private, Public, None }             SCOPE;
 
 typedef struct {
-  char *file;
+  const char *file;
   bool  userDef;
 } Include;
 
 typedef struct {
   TYPE  type;
-  char *name;
-  char *value;
-  int   address
+  const char *name;
+  const char *value;
+  int   address = -1;
 } Variable;
 
 typedef struct {
   SCOPE scope;
   TYPE  type;
-  char *name;
+  const char *name;
   int   localVars;
   int   globalVars;
 } Function;
--- Compiler/parser.cpp
+++ Compiler/parser.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 
+#include "abstractSyntaxTree.h"
+
 using namespace std;
 
 class Parser {
 public:
 
   // Constructor
-  Parser(string inFile, string outFile);
+  Parser(string inFile,
+         string outFile);
   ~Parser();
 
   // Variables
 
   // Functions
-  void add(Variable a, Variable b, Variable r);
+  void iadd(Variable &a,
+            Variable &b,
+            Variable &r);
 
 private:
 
@@ -22,45 +27,74 @@ private:
   // Address
   int returnAdd, currentAdd;
   // Jasmin strings
-  string addStr, storeStr, loadStr;
+  string addStr,
+         storeStr, loadStr, ldcStr;
 
   // Functions
-  void store(Variable a);
-  void load(Variable a);
+  void ldc(Variable &a);
+  void istore(Variable &a);
+  void iload(Variable &a);
 };
 
 // Constructor
 Parser::Parser(string inFile, string outFile) {
-  this->inFile = inFile;
+  this->inFile  = inFile;
   this->outFile = outFile;
 
-  this->returnAdd = 0;
+  this->returnAdd  = 0;
   this->currentAdd = 0;
 }
 
 Parser::~Parser() {}
 
-void Parser::add(Variable a, Variable b, Variable r){
-  load(a);
-  load(b);
+void Parser::iadd(Variable &a, Variable &b, Variable &r) {
+  iload(a);
+  iload(b);
+  addStr = "\t\t iadd\n";
+  printf("%s", addStr.c_str());
+  istore(r);
+}
 
-  store(r)
+// Push value to stack so it can be saved
+void Parser::ldc(Variable &a) {
+  ldcStr = "\t\t ldc " + string(a.value) + "\n";
+  printf("%s", ldcStr.c_str());
 }
 
 // Gets the value from the stack and save it to the address
-void Parser::store(Variable a){
-  currentAdd++;
-  storeStr = "\t\t istore " + currentAdd + "\n";
+void Parser::istore(Variable &a) {
+  if (a.address == -1) {
+    ldc(a);
+    a.address = ++currentAdd;
+  }
+  storeStr = "\t\t istore " + to_string(a.address) + "\n";
+  printf("%s", storeStr.c_str());
 }
+
 // Puts the value into the stack from the address
-void Parser::load(Variable a){
-  loadStr = "\t\t iload " + currentAdd + "\n";
+void Parser::iload(Variable &a) {
+  if (a.address == -1) {
+    istore(a);
+    // printf("a.address: %i, currentAdd: %i\n", a.address, currentAdd);
+  }
+  loadStr = "\t\t iload " + to_string(a.address) + "\n";
+  printf("%s", loadStr.c_str());
 }
 
 // Main function for the program
 int main(void) {
-  Parser p("../C-Program/add_num.c");
-  p.add(int a, int b);
+  Parser   p("../C-Program/add_num.c", "../C-Program/add_num.j");
+  Variable a, b;
+
+  a.type  = Int;
+  b.type  = Int;
+  a.name  = "x";
+  b.name  = "y";
+  a.value = "10";
+  b.value = "1";
+  // a.address = Int;
+  // b.address = Int;
+  p.iadd(a, b, a);
 
   return 0;
 }
